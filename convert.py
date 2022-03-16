03390ef70e4c46f851e7314baaef6e2522081b60
Shenghao Yang <me@shenghaoyang.info>
2022-03-16 21:52:39 +0800

    convert: remove external dependencies

--- convert.py
+++ convert.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 
 import sys
-from bitarray import bitarray
-from bitarray.util import ba2int
+import os
 
 WORD_SIZE = 18
 
 if __name__ == "__main__":
+    if os.isatty(sys.stdin.fileno()):
+        raise RuntimeError(
+            "refusing to read from TTY STDIN. Redirect input from a file instead."
+        )
+
     asm = sys.stdin.read()
     if len(asm) % WORD_SIZE:
-        raise RuntimeError("not an integer number of instruction words")
+        raise RuntimeError("input does not have an integer number of instruction words")
 
     words = len(asm) // WORD_SIZE
-    asm_bits = bitarray(asm)
 
     print("memory_initialization_radix=16;")
     print("memory_initialization_vector=", end="")
     for i in range(words):
         last_word = i == ((words - 1))
-        word = asm_bits[i * WORD_SIZE : (i + 1) * WORD_SIZE]
-        int_word = ba2int(word)
+        word = asm[i * WORD_SIZE : (i + 1) * WORD_SIZE]
+        int_word = int(word, 2)
         hex_word = hex(int_word)[2:]
 
         print(hex_word, end="," if not last_word else ";\n")
